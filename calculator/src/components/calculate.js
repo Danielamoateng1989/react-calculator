7e03533547d1d0d022946a484ad515ebd9615141
Daniel Amoateng <Danielamoateng81@yahoo.com>
2021-06-06 16:50:31 -0400

    Setup add, multiply, substraction and division functions in calculate js file

--- calculator/src/components/calculate.js
+++ calculator/src/components/calculate.js
@@ -1,17 +1,70 @@
-import React from 'react'
-import { Container }from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Container } from 'react-bootstrap'
 
 
-const calculate = () => {
+const Calculate = () => {
+  
+    const [currentSum,setCurrentSum]=useState(0);
+    const Add=(e)=>{
+        e.preventDefault();
+            let number1 = document.querySelector('#num1').value
+            let number2 = document.querySelector('#num2').value
+
+            let result = Number(number1) + Number(number2)
+            console.log(result)
+            setCurrentSum(result)
+
+        }
+
+    const multiply=(e)=>{
+        e.preventDefault();
+        let number1 = document.querySelector('#num1').value
+        let number2 = document.querySelector('#num2').value
+        
+        let result = number1 * number2
+        console.log(result)
+          
+      }
+    
+      const subtract=(e)=>{
+        e.preventDefault();
+        let number1 = document.querySelector('#num1').value
+        let number2 = document.querySelector('#num2').value
+        
+        let result = number1 - number2
+        console.log(result)
+          
+      }
+    
+      const divide=(e)=>{
+        e.preventDefault();
+        let number1 = document.querySelector('#num1').value
+        let number2 = document.querySelector('#num2').value
+        
+        let result = number1 / number2
+        console.log(result)
+          
+      }
+
     return (
         <>
-        <Container className="nav" >
+        <Container >
         <h2 className="center-text">React-Calculator</h2>
         </Container>
-      
-
+        <form>
+        <button onClick={Add}>+</button>
+            <button onClick={subtract}>-</button>
+            <button onClick={multiply}>*</button>
+            <button onClick={divide}>/</button>
+            <button>Clear</button>
+            <input type="text" id="num1" placeholder="enter a number" />
+            <input type="text" id="num2" placeholder="enter a number" />
+            <input type="text" id="result" readOnly />   
+            
+      </form>
+       
         </>
     )
 }
 
-export default calculate
+export default Calculate
