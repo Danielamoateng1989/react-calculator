52695f18d8eb90ced9fe30a77c7dc24f68cf68a9
Daniel Amoateng <Danielamoateng81@yahoo.com>
2021-06-06 15:33:23 -0400

    Added google font to make content look good

--- calculator/src/components/calculate.js
+++ calculator/src/components/calculate.js
@@ -5,8 +5,9 @@ import { Container }from 'react-bootstrap'
 const calculate = () => {
     return (
         <>
-        
+        <Container className="nav" >
         <h2 className="center-text">React-Calculator</h2>
+        </Container>
       
 
         </>
